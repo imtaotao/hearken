f305f46b7cb3895b51035a540974ba6d44197ca1
imtaotao <dwchentao@163.com>
2019-01-08 20:01:14 +0800

    fix: delete voice change file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import Grass from '@rustle/grass'
-// import M from './dev/partical.js'
-import M from './dev/voice-changer'
+import M from './dev/partical.js'
 // import M from './dev/media.grs'
 
 Grass.mount(document.getElementById('root'), M)
